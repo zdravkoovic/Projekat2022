2c1ae32ae726c498a03aaad35c18a996fd5d0770
Zdravković <zdravkoovic.nikola@elfak.rs>
2022-03-17 03:05:25 +0100

    Zavrseno!

--- Client/main.js
+++ Client/main.js
@@ -1,8 +1,4 @@
 import { Projekti } from "./Projekti.js";
 
-var Prikaz = document.createElement("div");
-Prikaz.className = "Prikaz";
-document.body.appendChild(Prikaz);
-
 var projekti = new Projekti();
 projekti.crtaj(document.body);
